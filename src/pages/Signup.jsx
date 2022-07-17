fff9bcc1b53b3335b2e088c2b1cc267d8c1ac64f
Carlos <kdela.rocks@gmail.com>
2022-07-16 23:27:56 -0500

    initial code to register users

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,3 +1,39 @@
+import { useState } from "react"
+
 export default function Signup(){
-    return <h1>Signup</h1>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signup = async (e) => {
+        e.preventDefault();
+        console.log(email, password);
+    }
+
+    return (
+        <div>
+            <h1>Signup</h1>
+            <form onSubmit={signup}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" 
+                    name="email" 
+                    id="email" 
+                    required
+                    onInput={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" 
+                    name="password" 
+                    id="password"
+                    required
+                    minLength="6"
+                    onInput={(e) => setPassword(e.target.value)}/>
+                </div>
+                <div>
+                    <button>Signup</button>
+                </div>
+            </form>
+        </div>
+    )
 }
