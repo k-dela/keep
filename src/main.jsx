b6bf76fd7202f426e002b0a8b047cca0180ac719
Carlos <kdela.rocks@gmail.com>
2022-07-20 15:47:27 -0500

    moved router to App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <BrowserRouter >
-    <Routes>
-      <Route path="/" element={ <Home />} />
-      <Route path="/login" element={ <Login />} />
-      <Route path="/signup" element={ <Signup />} />
-    </Routes>
-   </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
