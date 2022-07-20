cc5c758d13aa5072b35410a176c049ad32507346
Carlos <kdela.rocks@gmail.com>
2022-07-20 13:53:59 -0500

    can register users on db

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react"
+import supabase from "../supabase/supabase";
 
 export default function Signup(){
+    //console.log(import.meta.env.VITE_SUPABASE_URL);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const signup = async (e) => {
         e.preventDefault();
         console.log(email, password);
+
+        const {user, session, error} = await supabase.auth.signUp({
+            email,
+            password
+        });
+
+
+        if(error){
+            console.error(error);
+        }
+
+        console.log(user);
     }
 
     return (
