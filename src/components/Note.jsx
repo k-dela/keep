02acb91d03899326af2109ab6669ddad2f0a46c8
Carlos <kdela.rocks@gmail.com>
2022-07-22 11:56:53 -0500

    prevent modal from opening when deleting a note

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,6 +1,11 @@
 export default function Note({ note, deleteNote, handleClick }){
     return (
-        <div className='note' onClick={() =>{handleClick(note)}}>
+        <div className='note' onClick={(e) =>{
+            //console.log(e.target.tagName)
+            if(e.target.tagName != 'BUTTON'){
+                handleClick(note)
+            }
+            }}>
             <h2>{note.title}</h2>
             <p>{note.body}</p>
             <p>{note.id}</p>
