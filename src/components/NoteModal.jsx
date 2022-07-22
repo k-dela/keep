f4c51df9da3a56eb2dcddd20219fef4027cda82a
Carlos <kdela.rocks@gmail.com>
2022-07-22 11:13:38 -0500

    modal closes when clicking outside of it

--- src/components/NoteModal.jsx
+++ src/components/NoteModal.jsx
@@ -3,8 +3,8 @@ export default function NoteModal({isOpen, close}){
     if(isOpen === false) return null;
 
     return(
-        <div className='overlay'>
-            <div className='modal-container'>
+        <div className='overlay' onClick={close}>
+            <div className='modal-container' onClick={(e) => e.stopPropagation()}>
                 <h2>You clicked a note</h2>
                 <p>More to be announced</p>
                 <button onClick={close}>Close</button>
